13d4e81bd33d812ab22b62c815464432dca73dcf
Laurenz Schmidt <laurenzschmidt214@gmail.com>
2024-11-10 10:09:13 +0100

    fix while loop

--- installscript.sh
+++ installscript.sh
@@ -127,7 +127,7 @@ main() {
     _identify_os  # Call the function to identify OS
 #    echo "Please enter the name of the user who should receive the configfiles"
 #    read -r setupFor
-#    while true; do
+    while true; do
         read -rp "Do you also want to install hyprland? (yes/no) " yn
         case $yn in
             [Yy]* ) 
