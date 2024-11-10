b24e0fd32fae577d8d1d2e0ffca18c9ee83e6b64
Laurenz Schmidt <laurenzschmidt214@gmail.com>
2024-11-10 12:35:36 +0100

    add support for root user

--- installscript.sh
+++ installscript.sh
@@ -101,13 +101,23 @@ _pullGitrepository(){
     if [ -d dotfiles ]; then
         sudo rm -rf dotfiles
     fi
+
     git clone https://github.com/laugenbrezel1004/dotfiles.git
     echo "Overwriting existing configfiles"
-	sudo  rm -rf /home/$USER/.config/{bat,btop,cava,foot,kitty,lsd,neofetch,nvim,vim,ranger}
-	sudo  rm -rf /home/$USER/{.aliases,.tmux.conf,.vimrc,.zshrc}
-	sudo  mv -f dotfiles/{bat,btop,cava,foot,kitty,lsd,neofetch,ranger,nvim} /home/$USER/.config/
-	sudo  mv -f dotfiles/{.aliases,.tmux.conf,.vimrc,.zshrc} /home/$USER/
-	echo 
+	
+    if [ "$USER" = "root" ]; then
+	    rm -rf $USER/.config/{bat,btop,cava,foot,kitty,lsd,neofetch,nvim,vim,ranger}
+		rm -rf /$USER/{.aliases,.tmux.conf,.vimrc,.zshrc}
+		mv -f dotfiles/{bat,btop,cava,foot,kitty,lsd,neofetch,ranger,nvim} /$USER/.config/
+		mv -f dotfiles/{.aliases,.tmux.conf,.vimrc,.zshrc} /$USER/
+	else    
+	    sudo  rm -rf /home/$USER/.config/{bat,btop,cava,foot,kitty,lsd,neofetch,nvim,vim,ranger}
+		sudo  rm -rf /home/$USER/{.aliases,.tmux.conf,.vimrc,.zshrc}
+		sudo  mv -f dotfiles/{bat,btop,cava,foot,kitty,lsd,neofetch,ranger,nvim} /home/$USER/.config/
+		sudo  mv -f dotfiles/{.aliases,.tmux.conf,.vimrc,.zshrc} /home/$USER/
+	fi
+
+    echo 
     echo "You are all set!!!"
     neofetch
     exit 0
