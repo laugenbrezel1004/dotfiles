99c8b63b23f3158fe297ae436d17d149311acb64
Laurenz Schmidt <laurenzschmidt214@gmail.com>
2025-03-10 16:10:57 +0100

    rework install script

--- installationscripts/installscript.sh
+++ installationscripts/installscript.sh
@@ -1,39 +1,35 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 #TODO
 # install not everything with root in the users home dir xD.
 #zsh plugin, nvim plugin
 # Abort at error
 set -e
+bash -x
 # Set global variables
 os=""
 setupFor=""
 installer=""
-dependencies=("git" "dialog")
-installSoftware=("bat" "btop" "wget" "curl" "kitty" "thefuck" "lsd" "neofetch" "neovim" "vim" "zsh" "ranger" "tmux")
+dependencies=("git")
+installSoftware_emerge=("bat" "btop" "wget" "curl" "kitty" "thefuck" "lsd" "neofetch" "neovim" "vim" "zsh" "ranger" "tmux")
+installSoftware_apt=("bat" "btop" "wget" "curl" "kitty" "thefuck" "lsd" "neofetch" "neovim" "vim" "zsh" "ranger" "tmux")
 # installHyprlandSoftware=("mpv" "pulse" "swaync" "waybar" "wofi" "cava")
 # installHyprland=false
 # Get the OS 
 _identify_os() {
 	
-	dialog --title 'Installation Wizard' --msgbox 'Lets start with the installation!' 6 40 
-	
-	
-	for i in $(seq 1 100); do
-	  echo $i
-	  sleep 0.01
-	done | dialog --gauge "Checking OS type..." 10 70 0
-	
+	echo "Lets start with the installation!"
+	echo "Checking OS-Type"
 
     # Check for Gentoo
     if [ -f /etc/gentoo-release ]; then
         os="gentoo"
         installer="emerge"
-        dialog --title 'OS' --msgbox "Found $os as OS and $installer as packagemanager" 6 40 
+        echo "Found $os as OS and $installer as packagemanger"
         return
     elif grep -qi gentoo /etc/os-release &> /dev/null ; then
         os="gentoo"
         installer="emerge"
-        dialog --title 'OS' --msgbox "Found $os as OS and $installer as packagemanger" 6 40
+        echo "Found $os as OS and $installer as packagemanger"
         return
     fi
 
@@ -41,12 +37,12 @@ _identify_os() {
     if [ -f /etc/arch-release ]; then
         os="arch"
         installer="pacman"
-        dialog --title 'OS' --msgbox "Found $os as OS and $installer as packagemanger" 6 40
+        echo "Found $os as OS and $installer as packagemanger"
         return
     elif grep -qi arch /etc/os-release &> /dev/null ; then
         os="arch"
         installer="pacman"
-        dialog --title 'OS' --msgbox "Found $os as OS and $installer as packagemanger" 6 40
+        echo "Found $os as OS and $installer as packagemanger"
         return 
     fi
 
@@ -54,13 +50,13 @@ _identify_os() {
     if [ -f /etc/ubuntu-release ]; then
         os="ubuntu"
         installer="apt"
-        dialog --title 'OS' --msgbox "Found $os as OS and $installer as packagemanger" 6 40
+        echo "Found $os as OS and $installer as packagemanger"
         return
 
     elif grep -qi ubuntu /etc/os-release &> /dev/null ; then
         os="ubuntu"
         installer="apt"
-        dialog --title 'OS' --msgbox "Found $os as OS and $installer as packagemanger" 6 40
+        echo "Found $os as OS and $installer as packagemanger"
         return
     fi
 
@@ -68,21 +64,19 @@ _identify_os() {
     if [ -f /etc/debian-release ]; then
         os="debian"
         installer="apt"
-        dialog --title 'OS' --msgbox "Found $os as OS and $installer as packagemanger" 6 40
+        echo "Found $os as OS and $installer as packagemanger"
         return
     elif grep -qi debian /etc/os-release &> /dev/null ; then
         os="debian"
         installer="apt"
-        dialog --title 'OS' --msgbox "Found $os as OS and $installer as packagemanger" 6 40
+        echo "Found $os as OS and $installer as packagemanger"
         return
     fi
 
-if [ -z "$os" ]; then  # If 'os' is empty, it means no OS was found
+    if [ -z "$os" ]; then  # If 'os' is empty, it means no OS was found
         echo "Unable to find OS type, aborting the script!!!"
         exit 1  # Exit with a non-zero status code to indicate failure
     fi
-
-
 }
 
 #install software and dependencies
@@ -90,22 +84,15 @@ _installsoftware(){
     if [ "$os" = "gentoo" ]; then
 	   
 	      sudo emaint --yes sync --all &> /dev/null
-        for i in $(seq 1 100); do
-		    echo $i
-		    sleep 0.2
-		    done | dialog --gauge "Updating portage repository" 10 70 0
+		    echo "Updating portage repository" 
 	        for i in "${installSoftware[@]}"; do
 	            sudo emerge "$i"
 	        done 
 	fi
 
     if [ "$os" = "debian" ] || [ "$os" = "ubuntu" ]; then
-
+      echo "Updating apt repositories"
 	   sudo apt update -y &> /dev/null
-       for i in $(seq 1 100); do
-		    echo $i
-		    sleep 0.2
-       done | dialog --gauge "Updating apt repository" 10 70 0
 	        for i in "${installSoftware[@]}"; do
 	            sudo apt install -y "$i"
 	        done 
@@ -117,24 +104,23 @@ _installsoftware(){
 }
 
 _pullGitrepository(){
+    echo "Backuping up .config direcotry"
+    sleep 3
+    mv .config .config.bac
     echo "Pulling down the git repository"
-    cd /tmp
-    if [ -d dotfiles ]; then
-        sudo rm -rf dotfiles
-    fi
+
 
     git clone https://github.com/laugenbrezel1004/dotfiles.git
     echo "Overwriting existing configfiles"
-    if [ "$USER" = "root" ]; then
-	  rm -rf /$USER/.config/{bat,btop,cava,foot,kitty,lsd,neofetch,nvim,vim,ranger}
-		rm -rf /$USER/{.aliases,.tmux.conf,.vimrc,.zshrc}
-		mv -f dotfiles/{bat,btop,cava,foot,kitty,lsd,neofetch,ranger,nvim} /$USER/.config/
-		mv -f dotfiles/{.aliases,.tmux.conf,.vimrc,.zshrc} /$USER/
+    if [  "$USER" = "root" ]; then
+      sudo mkdir $USER/.config.backup
+      sudo mv -f $USER/.config/{bat,btop,kitty,lsd,neofetch,ranger,nvim} $USER/.config.backup
+		  sudo mv -f dotfiles/{bat, btop, kitty, lsd, neofetch, nvim, ranger, qimgv, ranger, starship, thefuck, tmux, zsh} $USER/.config/
 	else    
-	  sudo  rm -rf /home/$USER/.config/{bat,btop,cava,foot,kitty,lsd,neofetch,nvim,vim,ranger}
-		sudo  rm -rf /home/$USER/{.aliases,.tmux.conf,.vimrc,.zshrc}
-		sudo  mv -f dotfiles/{bat,btop,cava,foot,kitty,lsd,neofetch,ranger,nvim} /home/$USER/.config/
-		sudo  mv -f dotfiles/{.aliases,.tmux.conf,.vimrc,.zshrc} /home/$USER/
+		  # rm -rf /home/$USER/{.aliases,.tmux.conf,.vimrc,.zshrc}
+      sudo mkdir $USER/.config.backup
+      sudo mv -f $USER/.config/{bat,btop,kitty,lsd,neofetch,ranger,nvim} $USER/.config.backup
+		  sudo mv -f dotfiles/{bat, btop, kitty, lsd, neofetch, nvim, ranger, qimgv, ranger, starship, thefuck, tmux, zsh} $USER/.config/
 	fi
 
     echo 
@@ -144,18 +130,10 @@ _pullGitrepository(){
     exit 0
 }
 _checkFundamentalSoftware(){
-        missing_deps=false
-if ! which git &> /dev/null ; then
-    echo "Please install git"
-    missing_deps=true
-fi
-if ! which dialog &> /dev/null ; then
-    echo "Please install dialog"
-    missing_deps=true
-fi
-if $missing_deps ; then
-    exit 1
-fi
+    if ! which git &> /dev/null ; then
+        echo "Please install git"
+        exit 1
+    fi
 }
 main() {
 #    currentUser=$(whoami)
@@ -163,8 +141,8 @@ main() {
 #
     _checkFundamentalSoftware # check if Fundamental are one the system 
     _identify_os  # Call the function to identify OS
-    _installsoftware # install the needed software 
     _pullGitrepository # download the git repo
+    _installsoftware # install the needed software 
 }
 
 main  # Call main function
