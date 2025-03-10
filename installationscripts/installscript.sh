14bfe30264a42adcccdf29a1e637ce8976c8f1d0
Laurenz Schmidt <laurenzschmidt214@gmail.com>
2025-03-10 17:01:54 +0100

    update install script

--- installationscripts/installscript.sh
+++ installationscripts/installscript.sh
@@ -5,12 +5,11 @@
 # Abort at error
 set -e
 # Set global variables
-os=""
-setupFor=""
-installer=""
-dependencies=("git")
-installSoftware_emerge=("bat" "btop" "wget" "curl" "kitty" "thefuck" "lsd" "neofetch" "neovim" "vim" "zsh" "ranger" "tmux")
-installSoftware_apt=("bat" "btop" "wget" "curl" "kitty" "thefuck" "lsd" "neofetch" "neovim" "vim" "zsh" "ranger" "tmux")
+_os=""
+_setupFor=""
+_installer=""
+_installSoftware_emerge=("bat" "btop" "wget" "curl" "kitty" "thefuck" "lsd" "neofetch" "neovim" "vim" "zsh" "ranger" "tmux")
+_installSoftware_apt=("bat" "btop" "wget" "curl" "kitty" "thefuck" "lsd" "neofetch" "neovim" "vim" "zsh" "ranger" "tmux")
 # installHyprlandSoftware=("mpv" "pulse" "swaync" "waybar" "wofi" "cava")
 # installHyprland=false
 # Get the OS 
@@ -21,58 +20,58 @@ _identify_os() {
 
     # Check for Gentoo
     if [ -f /etc/gentoo-release ]; then
-        os="gentoo"
-        installer="emerge"
-        echo "Found $os as OS and $installer as packagemanger"
+        _os="gentoo"
+        _installer="emerge"
+        echo "Found $_os as OS and $_installer as packagemanger"
         return
     elif grep -qi gentoo /etc/os-release &> /dev/null ; then
-        os="gentoo"
-        installer="emerge"
-        echo "Found $os as OS and $installer as packagemanger"
+        _os="gentoo"
+        _installer="emerge"
+        echo "Found $_os as OS and $_installer as packagemanger"
         return
     fi
 
     # Check for Arch
     if [ -f /etc/arch-release ]; then
-        os="arch"
-        installer="pacman"
-        echo "Found $os as OS and $installer as packagemanger"
+        _os="arch"
+        _installer="pacman"
+        echo "Found $_os as OS and $_installer as packagemanger"
         return
     elif grep -qi arch /etc/os-release &> /dev/null ; then
-        os="arch"
-        installer="pacman"
-        echo "Found $os as OS and $installer as packagemanger"
+        _os="arch"
+        _installer="pacman"
+        echo "Found $_os as OS and $_installer as packagemanger"
         return 
     fi
 
     # Check for Ubuntu 
     if [ -f /etc/ubuntu-release ]; then
-        os="ubuntu"
-        installer="apt"
-        echo "Found $os as OS and $installer as packagemanger"
+        _os="ubuntu"
+        _installer="apt"
+        echo "Found $_os as OS and $_installer as packagemanger"
         return
 
     elif grep -qi ubuntu /etc/os-release &> /dev/null ; then
-        os="ubuntu"
-        installer="apt"
-        echo "Found $os as OS and $installer as packagemanger"
+        _os="ubuntu"
+        _installer="apt"
+        echo "Found $_os as OS and $_installer as packagemanger"
         return
     fi
 
     # Check for Debian 
     if [ -f /etc/debian-release ]; then
-        os="debian"
-        installer="apt"
-        echo "Found $os as OS and $installer as packagemanger"
+        _os="debian"
+        _installer="apt"
+        echo "Found $_os as OS and $_installer as packagemanger"
         return
     elif grep -qi debian /etc/os-release &> /dev/null ; then
-        os="debian"
-        installer="apt"
-        echo "Found $os as OS and $installer as packagemanger"
+        _os="debian"
+        _installer="apt"
+        echo "Found $_os as OS and $_installer as packagemanger"
         return
     fi
 
-    if [ -z "$os" ]; then  # If 'os' is empty, it means no OS was found
+    if [ -z "$_os" ]; then  # If '_os' is empty, it means no OS was found
         echo "Unable to find OS type, aborting the script!!!"
         exit 1  # Exit with a non-zero status code to indicate failure
     fi
@@ -80,19 +79,19 @@ _identify_os() {
 
 #install software and dependencies
 _installsoftware(){
-    if [ "$os" = "gentoo" ]; then
+    if [ "$_os" = "gentoo" ]; then
 	   
 	      sudo emaint --yes sync --all &> /dev/null
 		    echo "Updating portage repository" 
-	        for i in "${installSoftware[@]}"; do
+	        for i in "${_installSoftware_emerge[@]}"; do
 	            sudo emerge "$i"
 	        done 
 	fi
 
-    if [ "$os" = "debian" ] || [ "$os" = "ubuntu" ]; then
+    if [ "$_os" = "debian" ] || [ "$_os" = "ubuntu" ]; then
       echo "Updating apt repositories"
 	   sudo apt update -y &> /dev/null
-	        for i in "${installSoftware[@]}"; do
+	        for i in "${_installSoftware_apt[@]}"; do
 	            sudo apt install -y "$i"
 	        done 
     fi
@@ -103,60 +102,61 @@ _installsoftware(){
 }
 
 _pullGitrepository(){
+
     echo "Backuping up .config direcotry"
     if [ ! -d "$HOME/.config.backup" ]; then
       mkdir $HOME/.config.backup
     fi
      
     #backup old config
-    
-  if [ -d "$HOME/.config/bat" ]; then
-    mv -f "$HOME/.config/bat" "$HOME/.config.backup"
-  fi
+      
+    if [ -d "$HOME/.config/bat" ]; then
+      mv -f "$HOME/.config/bat" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/btop" ]; then
-      mv -f "$HOME/.config/btop" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/btop" ]; then
+        mv -f "$HOME/.config/btop" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/kitty" ]; then
-      mv -f "$HOME/.config/kitty" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/kitty" ]; then
+        mv -f "$HOME/.config/kitty" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/lsd" ]; then
-      mv -f "$HOME/.config/lsd" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/lsd" ]; then
+        mv -f "$HOME/.config/lsd" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/neofetch" ]; then
-      mv -f "$HOME/.config/neofetch" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/neofetch" ]; then
+        mv -f "$HOME/.config/neofetch" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/nvim" ]; then
-      mv -f "$HOME/.config/nvim" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/nvim" ]; then
+        mv -f "$HOME/.config/nvim" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/ranger" ]; then
-      mv -f "$HOME/.config/ranger" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/ranger" ]; then
+        mv -f "$HOME/.config/ranger" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/qimgv" ]; then
-      mv -f "$HOME/.config/qimgv" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/qimgv" ]; then
+        mv -f "$HOME/.config/qimgv" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/starship" ]; then
-      mv -f "$HOME/.config/starship" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/starship" ]; then
+        mv -f "$HOME/.config/starship" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/thefuck" ]; then
-      mv -f "$HOME/.config/thefuck" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/thefuck" ]; then
+        mv -f "$HOME/.config/thefuck" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/tmux" ]; then
-      mv -f "$HOME/.config/tmux" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/tmux" ]; then
+        mv -f "$HOME/.config/tmux" "$HOME/.config.backup"
+    fi
 
-  if [ -d "$HOME/.config/zsh" ]; then
-      mv -f "$HOME/.config/zsh" "$HOME/.config.backup"
-  fi
+    if [ -d "$HOME/.config/zsh" ]; then
+        mv -f "$HOME/.config/zsh" "$HOME/.config.backup"
+    fi
     if [ -f "$HOME/.aliases" ]; then
       sudo mv "$HOME/.aliases"  "$HOME/.config.backup/"
     fi
