8f362b52dfe28a6aedec6e131c662b34abd4a6e6
laurenz <laurenzschmidt214@gmail.com>
2025-07-06 17:04:01 +0200

    check working dir

--- install.sh
+++ install.sh
@@ -18,11 +18,31 @@ echo "installing dotfiles..."
 if [ ! $UID -eq 0 ]; then
   echo "You need to run this script as root"
   echo "aborting..."
+  exit 1
 fi
 
 
 apt update -y 
 
 for package in "${__apt_packages[@]}"; do
-  apt install "$package"
+  apt install "$package" -y
 done
+
+
+
+## install dotfiles to correct dir
+## backing up old dotfiles
+
+if [ -f ./install.sh ]; then
+  echo "you are in the right dir"
+fi
+
+if [ ! -f ./install.sh ]; then
+  echo "wrong workingdir"
+  echo "aborting..."
+  exit 1
+fi
+
+
+
+
